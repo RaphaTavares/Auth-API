7568d11d1d17647bf35e27cdca076727899c6434
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-08-24 00:48:47 -0300

    Add constraints to req inputs [wip]

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,35 @@
 const User = require('../models/user');
 
 const user_create_post = (req, res) => {
-    const userModel = new User(req.body);
 
-    userModel.save()
-        .then(() => {
-            res.status(201).json({ response: 'ok' });
-        })
-        .catch(err => console.log(err));
+    if(validaPayload(req)){
+        const userModel = new User(req.body);
+
+        userModel.save()
+            .then(() => {
+                res.status(201).json({ response: 'ok' });
+            })
+            .catch(err => console.log(err));
+}
+    }
+
+    
+const validaPayload = (req) => {
+    const reqName = req.body.name;
+    const reqSurname = req.body.surname;
+    const reqEmail = req.body.email;
+    const reqUser = req.body.user;
+    //const { password } = req.body;
+    const regexName = new RegExp(/^[A-Za-z]+$/);
+
+    if(reqName.match(regexName))
+    {
+        if(reqSurname.match(regexName)){
+            if(password.length >= 6){
+                return true;
+            }
+        }
+    }
 }
 
 module.exports = {
