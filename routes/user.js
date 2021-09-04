896b5638ba484e9a2c9b28606f6d19c6fd43fad5
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-09-04 09:36:10 -0300

    Delete user routes

--- routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.post('/', userController.user_create_post);
-
-module.exports = router;
