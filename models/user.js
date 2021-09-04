0747bc3d666a5d7ceed30045df7499c5e83bb69b
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-09-04 09:34:58 -0300

    Add error messages and pre save function to hash password

--- models/user.js
+++ models/user.js
@@ -1,28 +1,40 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Please enter a name']
     },
     surname: {
         type: String,
-        required: true
+        required: [true, 'Please enter a surname']
     },
     email: {
         type: String,
-        required: true
+        required: [true, 'Please enter an email'],
+        unique: true,
+        validate: [isEmail, 'Please enter a valid email']
     },
     user: {
         type: String,
-        required: true
+        required: [true, 'Please enter a username'],
+        unique: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: [6, 'Password must be at least 6 characters']
     }
 }, { timestamps: true })
+
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
 const User = mongoose.model('User', userSchema);
 
