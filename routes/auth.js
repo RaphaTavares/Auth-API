7ca3dd9a9e23c8efd7ff3530304a2341a44dd4ec
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-09-04 09:34:23 -0300

    Comment not implemented routes and refactor signup

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/', authController.auth_post);
+router.post('/signup', authController.signup_post);
+
+//router.post('/', authController.login_post);
+
+//router.post('/', authController.logout_post);
 
 module.exports = router;
