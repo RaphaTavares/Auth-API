0edfab7d36298b8890748201632416dfc25162fc
Raphael Tavares <raphael_tavares2000@hotmail.com>
2021-09-11 02:08:48 -0300

    Change container name

--- InfraAsCode/authACR.tf
+++ InfraAsCode/authACR.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "ContainerAuth"
+  name                = "nodeauthacr"
   resource_group_name = data.azurerm_resource_group.pandora_box.name
   location            = data.azurerm_resource_group.pandora_box.location
   sku                 = "Standard"
