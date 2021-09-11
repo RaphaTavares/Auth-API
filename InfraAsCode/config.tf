8fef5907740c0efe32869c4623149d7830cf9f9d
Raphael Tavares <raphael_tavares2000@hotmail.com>
2021-09-10 23:57:43 -0300

    Update terraform azurerm version

--- InfraAsCode/config.tf
+++ InfraAsCode/config.tf
@@ -2,12 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "~> 2.65"
     }
   }
+
+  required_version = ">= 0.14.9"
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
